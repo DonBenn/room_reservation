0e9d6cb7373580ae6fc1bee6006fb890d45875fa
Denis Bessonov <denibessono@yandex.ru>
2025-04-24 18:57:59 +0300

    Migrations alembic apply

--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Text
 
-# Импортируем базовый класс для моделей.
 from app.core.db import Base
 
 
 class MeetingRoom(Base):
-    # Имя переговорки должно быть не больше 100 символов,
-    # уникальным и непустым.
     name = Column(String(100), unique=True, nullable=False)
+    # Новый атрибут модели. Значение nullable по умолчанию равно True,
+    # поэтому его можно не указывать.
+    description = Column(Text)

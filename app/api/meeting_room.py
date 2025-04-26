27c6f6045a2a252a987a41908a3ae239777593ea
Denis Bessonov <denibessono@yandex.ru>
2025-04-26 20:20:53 +0300

    Add Dependency Injection

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 
+# Импортируем класс асинхронной сессии для аннотации параметра.
+from sqlalchemy.ext.asyncio import AsyncSession
+
+# Импортируем асинхронный генератор сессий.
+from app.core.db import get_async_session
 from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
-# Импортируем схему ответа.
 from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB
 
 router = APIRouter()
@@ -9,18 +13,21 @@ router = APIRouter()
 
 @router.post(
     '/meeting_rooms/',
-    # Указываем схему ответа.
     response_model=MeetingRoomDB,
     response_model_exclude_none=True,
 )
 async def create_new_meeting_room(
         meeting_room: MeetingRoomCreate,
+        # Указываем зависимость, предоставляющую объект сессии, как параметр функции.
+        session: AsyncSession = Depends(get_async_session),
 ):
-    room_id = await get_room_id_by_name(meeting_room.name)
+    # Вторым параметром передаём сессию в CRUD-функцию:
+    room_id = await get_room_id_by_name(meeting_room.name, session)
     if room_id is not None:
         raise HTTPException(
             status_code=422,
             detail='Переговорка с таким именем уже существует!',
         )
-    new_room = await create_meeting_room(meeting_room)
-    return new_room
+    # Вторым параметром передаём сессию в CRUD-функцию:
+    new_room = await create_meeting_room(meeting_room, session)
+    return new_room 
--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,32 +1,37 @@
 from typing import Optional
 
-# Добавляем импорт функции select.
 from sqlalchemy import select
+# Импортируем класс асинхронной сессии для аннотаций.
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import MeetingRoomCreate
 
 
-async def create_meeting_room(new_room: MeetingRoomCreate) -> MeetingRoom:
+async def create_meeting_room(
+        new_room: MeetingRoomCreate,
+        # Добавляем новый параметр.
+        session: AsyncSession,
+) -> MeetingRoom:
     new_room_data = new_room.dict()
     db_room = MeetingRoom(**new_room_data)
-    async with AsyncSessionLocal() as session:
-        session.add(db_room)
-        await session.commit()
-        await session.refresh(db_room)
+    # Убираем контекстный менеджер.
+    session.add(db_room)
+    await session.commit()
+    await session.refresh(db_room)
     return db_room
 
 
-# Добавляем новую асинхронную функцию.
-async def get_room_id_by_name(room_name: str) -> Optional[int]:
-    async with AsyncSessionLocal() as session:
-        # Получаем объект класса Result.
-        db_room_id = await session.execute(
-            select(MeetingRoom.id).where(
-                MeetingRoom.name == room_name
-            )
+async def get_room_id_by_name(
+        room_name: str,
+        # Добавляем новый параметр.
+        session: AsyncSession,
+) -> Optional[int]:
+    # Убираем контекстный менеджер.
+    db_room_id = await session.execute(
+        select(MeetingRoom.id).where(
+            MeetingRoom.name == room_name
         )
-        # Извлекаем из него конкретное значение.
-        db_room_id = db_room_id.scalars().first()
+    )
+    db_room_id = db_room_id.scalars().first()
     return db_room_id

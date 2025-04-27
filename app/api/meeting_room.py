2ee0232a2776051b4fe56b0b8b227562d1a0c56d
Denis Bessonov <denibessono@yandex.ru>
2025-04-27 20:26:46 +0300

    Update, delete routers

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -5,8 +5,15 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 # Импортируем асинхронный генератор сессий.
 from app.core.db import get_async_session
-from app.crud.meeting_room import create_meeting_room, get_room_id_by_name, read_all_rooms_from_db
-from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB
+from app.crud.meeting_room import (
+    create_meeting_room, delete_meeting_room,
+    get_meeting_room_by_id, get_room_id_by_name,
+    read_all_rooms_from_db, update_meeting_room
+)
+from app.models.meeting_room import MeetingRoom
+from app.schemas.meeting_room import (
+    MeetingRoomCreate, MeetingRoomDB, MeetingRoomUpdate
+)
 
 router = APIRouter(prefix='/meeting_rooms')
 router = APIRouter(
@@ -21,17 +28,12 @@ router = APIRouter(
 )
 async def create_new_meeting_room(
         meeting_room: MeetingRoomCreate,
-        # Указываем зависимость, предоставляющую объект сессии, как параметр функции.
         session: AsyncSession = Depends(get_async_session),
 ):
-    # Вторым параметром передаём сессию в CRUD-функцию:
-    room_id = await get_room_id_by_name(meeting_room.name, session)
-    if room_id is not None:
-        raise HTTPException(
-            status_code=422,
-            detail='Переговорка с таким именем уже существует!',
-        )
-    # Вторым параметром передаём сессию в CRUD-функцию:
+    # Выносим проверку дубликата имени в отдельную корутину.
+    # Если такое имя уже существует, то будет вызвана ошибка HTTPException
+    # и обработка запроса остановится.
+    await check_name_duplicate(meeting_room.name, session)
     new_room = await create_meeting_room(meeting_room, session)
     return new_room
 
@@ -45,3 +47,75 @@ async def get_all_meeting_rooms(
 ):
     all_rooms = await read_all_rooms_from_db(session)
     return all_rooms
+
+
+@router.patch(
+    '/{meeting_room_id}',
+    response_model=MeetingRoomDB,
+    response_model_exclude_none=True,
+)
+async def partially_update_meeting_room(
+        meeting_room_id: int,
+        obj_in: MeetingRoomUpdate,
+        session: AsyncSession = Depends(get_async_session),
+):
+    # Выносим повторяющийся код в отдельную корутину.
+    meeting_room = await check_meeting_room_exists(
+        meeting_room_id, session
+    )
+
+    if obj_in.name is not None:
+        await check_name_duplicate(obj_in.name, session)
+
+    meeting_room = await update_meeting_room(
+        meeting_room, obj_in, session
+    )
+    return meeting_room
+
+
+# Корутина, проверяющая уникальность полученного имени переговорки.
+async def check_name_duplicate(
+        room_name: str,
+        session: AsyncSession,
+) -> None:
+    room_id = await get_room_id_by_name(room_name, session)
+    if room_id is not None:
+        raise HTTPException(
+            status_code=422,
+            detail='Переговорка с таким именем уже существует!',
+        )
+
+
+@router.delete(
+    '/{meeting_room_id}',
+    response_model=MeetingRoomDB,
+    response_model_exclude_none=True,
+)
+async def remove_meeting_room(
+        meeting_room_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    # Выносим повторяющийся код в отдельную корутину.
+    meeting_room = await check_meeting_room_exists(
+        meeting_room_id, session
+    )
+    meeting_room = await delete_meeting_room(
+        meeting_room, session
+    )
+    return meeting_room
+
+
+# Оформляем повторяющийся код в виде отдельной корутины.
+async def check_meeting_room_exists(
+        meeting_room_id: int,
+        session: AsyncSession,
+) -> MeetingRoom:
+    meeting_room = await get_meeting_room_by_id(
+        meeting_room_id, session
+    )
+    if meeting_room is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Переговорка не найдена!'
+        )
+    return meeting_room
--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -3,9 +3,10 @@ from typing import Optional, List
 from sqlalchemy import select
 # Импортируем класс асинхронной сессии для аннотаций.
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.encoders import jsonable_encoder
 
 from app.models.meeting_room import MeetingRoom
-from app.schemas.meeting_room import MeetingRoomCreate
+from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomUpdate
 
 
 async def create_meeting_room(
@@ -41,3 +42,56 @@ async def read_all_rooms_from_db(
 ) -> list[MeetingRoom]:
     db_rooms = await session.execute(select(MeetingRoom))
     return db_rooms.scalars().all()
+
+
+async def get_meeting_room_by_id(
+        room_id: int,
+        session: AsyncSession,
+) -> Optional[MeetingRoom]:
+    db_room = await session.execute(
+        select(MeetingRoom).where(
+            MeetingRoom.id == room_id
+        )
+    )
+    db_room = db_room.scalars().first()
+    return db_room
+
+
+async def update_meeting_room(
+        # Объект из БД для обновления.
+        db_room: MeetingRoom,
+        # Объект из запроса.
+        room_in: MeetingRoomUpdate,
+        session: AsyncSession,
+) -> MeetingRoom:
+    # Представляем объект из БД в виде словаря.
+    obj_data = jsonable_encoder(db_room)
+    # Конвертируем объект с данными из запроса в словарь,
+    # исключаем неустановленные пользователем поля.
+    update_data = room_in.dict(exclude_unset=True)
+
+    # Перебираем все ключи словаря, сформированного из БД-объекта.
+    for field in obj_data:
+        # Если конкретное поле есть в словаре с данными из запроса, то...
+        if field in update_data:
+            # ...устанавливаем объекту БД новое значение атрибута.
+            setattr(db_room, field, update_data[field])
+    # Добавляем обновленный объект в сессию.
+    session.add(db_room)
+    # Фиксируем изменения.
+    await session.commit()
+    # Обновляем объект из БД.
+    await session.refresh(db_room)
+    return db_room
+
+async def delete_meeting_room(
+        db_room: MeetingRoom,
+        session: AsyncSession,
+) -> MeetingRoom:
+    # Удаляем объект из БД.
+    await session.delete(db_room)
+    # Фиксируем изменения в БД.
+    await session.commit()
+    # Не обновляем объект через метод refresh(),
+    # следовательно он всё ещё содержит информацию об удаляемом объекте.
+    return db_room

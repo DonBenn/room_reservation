a8f858483a8873a6bf73f59032d85d6606f53e59
Denis Bessonov <denibessono@yandex.ru>
2025-04-28 19:35:54 +0300

    Routers refactoring, Reservation endpoint creation

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -8,11 +8,9 @@ from app.models.meeting_room import MeetingRoom
 from app.schemas.meeting_room import (
     MeetingRoomCreate, MeetingRoomDB, MeetingRoomUpdate
 )
+from app.api.validators import (check_name_duplicate, check_meeting_room_exists)
 
-router = APIRouter(
-    prefix='/meeting_rooms',
-    tags=['Meeting Rooms'],
-)
+router = APIRouter()
 
 
 @router.post(
@@ -25,7 +23,6 @@ async def create_new_meeting_room(
         session: AsyncSession = Depends(get_async_session),
 ):
     await check_name_duplicate(meeting_room.name, session)
-    # Замените вызов функции на вызов метода.
     new_room = await meeting_room_crud.create(meeting_room, session)
     return new_room
 
@@ -38,7 +35,6 @@ async def create_new_meeting_room(
 async def get_all_meeting_rooms(
         session: AsyncSession = Depends(get_async_session),
 ):
-    # Замените вызов функции на вызов метода.
     all_rooms = await meeting_room_crud.get_multi(session)
     return all_rooms
 
@@ -60,7 +56,6 @@ async def partially_update_meeting_room(
     if obj_in.name is not None:
         await check_name_duplicate(obj_in.name, session)
 
-    # Замените вызов функции на вызов метода.
     meeting_room = await meeting_room_crud.update(
         meeting_room, obj_in, session
     )
@@ -77,33 +72,5 @@ async def remove_meeting_room(
         session: AsyncSession = Depends(get_async_session),
 ):
     meeting_room = await check_meeting_room_exists(meeting_room_id, session)
-    # Замените вызов функции на вызов метода.
     meeting_room = await meeting_room_crud.remove(meeting_room, session)
-    return meeting_room
-
-
-async def check_name_duplicate(
-        room_name: str,
-        session: AsyncSession,
-) -> None:
-    # Замените вызов функции на вызов метода.
-    room_id = await meeting_room_crud.get_room_id_by_name(room_name, session)
-    if room_id is not None:
-        raise HTTPException(
-            status_code=422,
-            detail='Переговорка с таким именем уже существует!',
-        )
-
-
-async def check_meeting_room_exists(
-        meeting_room_id: int,
-        session: AsyncSession,
-) -> MeetingRoom:
-    # Замените вызов функции на вызов метода.
-    meeting_room = await meeting_room_crud.get(meeting_room_id, session)
-    if meeting_room is None:
-        raise HTTPException(
-            status_code=404,
-            detail='Переговорка не найдена!'
-        )
     return meeting_room

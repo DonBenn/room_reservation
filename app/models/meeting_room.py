e30832edd75b03e2fef4f84e99fc9329e5f73674
Denis Bessonov <denibessono@yandex.ru>
2025-04-28 16:14:08 +0300

    Add Reservation model

--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, String, Text
+from sqlalchemy.orm import relationship
 
 from app.core.db import Base
 
 
 class MeetingRoom(Base):
     name = Column(String(100), unique=True, nullable=False)
-    # Новый атрибут модели. Значение nullable по умолчанию равно True,
-    # поэтому его можно не указывать.
     description = Column(Text)
+    reservations = relationship('Reservation', cascade='delete')
